c82b90061df612193af469a941e1f2583f499ad1
Renan Gomes <92251856+renanvgomes@users.noreply.github.com>
2022-03-27 20:12:42 -0300

    Delete L1.6.py

--- L1.6.py
+++
@@ -1,6 +0,0 @@
-# Faça um algoritmo que calcule a área de um quadrado, sendo que o comprimento do lado é informado pelo usuário. 
-# A área do quadrado é calculada elevando-se o lado ao quadrado.
-print('Atividade 06')
-var1 = float(input('Digite o comprimento do lado o quadrado: '))
-print('Calculando a área do quadrado...')
-print('A área do quadrado será ', (var1 ** 2 ))
