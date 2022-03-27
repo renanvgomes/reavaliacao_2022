5a024d7187771d222a818f27458127cd76dd8de5
Renan Gomes <92251856+renanvgomes@users.noreply.github.com>
2022-03-27 20:12:48 -0300

    Delete L1.7.py

--- L1.7.py
+++
@@ -1,7 +0,0 @@
-# Faça um algoritmo que calcule a área de um retângulo, sendo que os comprimentos da altura e da base são informados pelo usuário. 
-# A área do retângulo é calculada multiplicando-se a altura pela base.
-print('Atividade 07')
-var1 = float(input('Digite o comprimento do retângulo: '))
-var2 = float(input('Digite a largura do retângulo: '))
-print('Calculando a área do retângulo...')
-print('A área do retângulo será ', (var1 * var2 ))
