cc753ca380b5e61638076b8b23e7cb0bc0fde1ec
Renan Gomes <92251856+renanvgomes@users.noreply.github.com>
2022-03-27 20:12:31 -0300

    Delete L1.4.py

--- L1.4.py
+++
@@ -1,4 +0,0 @@
-# Faça um algoritmo que calcule a gorjeta a ser paga de uma conta de restaurante. A gorjeta é calculada como sendo 10% do valor da conta, que deve ser informado pelo usuário.
-print('Atividade 04')
-var1 = float(input('Qual o valor da conta?: '))
-print('Total a ser pago com a gorjeta será: ', round(var1 * 1.10, 2))
