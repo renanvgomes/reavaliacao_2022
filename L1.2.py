dd06115c59b102bb483d87fe990d9ec075ce78b9
Renan Gomes <92251856+renanvgomes@users.noreply.github.com>
2022-03-27 20:12:18 -0300

    Delete L1.2.py

--- L1.2.py
+++
@@ -1,5 +0,0 @@
-# Faça um algoritmo para calcular a média de duas notas digitadas pelo usuário.
-print('Atividade 02')
-var1 = int(input('Digite a primeira nota: '))
-var2 = int(input('Digite a Segunda nota: '))
-print('A média é: ', (var1 + var2) / 2)
